0f5e2d6da55e8866d92bd79600a688a8729d9df3
RaphaTaito <raphaelrheibel@gmail.com>
2025-02-28 16:34:55 -0300

    [config] criação do projeto e início da criação de rotas

---
+++ angular-spotify/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        //configurando lazy loading
+        loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+    }
+];
---
+++ angular-spotify/src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+    pudction: true,
+    apiUrl: 'http://localhost:4200'
+};
+
+export const SpotifyConfiguration = {
+    clientId: '0a64bd4d77224a01beb9450130a4f5c5',
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+    redirectUrl: 'http://localhost:4200/login/',
+    scopes:[
+        "user-read-currently-playing",
+        "user-read-recently-played",
+        "user-read-playback-state",
+        "user-top-read",
+        "user-modify-playback-state",
+        "user-library-read",
+        "playlist-read-private",
+        "playlist-read-collaborative"
+    ]
+}
+
